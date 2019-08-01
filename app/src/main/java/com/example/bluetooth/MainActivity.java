bb7cd9c5b9ad3e49f8eb0ea479ea13b68a2c566d
rthunoli <rajendran.thunoli@gmail.com>
2019-08-01 21:51:36 +0400

    Bluetooth discovery code

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -3,59 +3,84 @@ package com.example.bluetooth;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    BluetoothAdapter bluetoothAdapter;
+    private BluetoothAdapter bluetoothAdapter;
+    private ListView devicesListView;
     private static final int ENABLE_BT_REQUEST_CODE = 1;
+    private static final String TAG = "MainActivity";
+    //private static final int REQUEST_DISCOVERABLE_CODE = 2;
+
+    private IntentFilter filter;
+    private ArrayAdapter<String> mArrayAdapter;
+
+    //Create a BroadcastReceiver for ACTION_FOUND//
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            //Whenever a remote Bluetooth device is found...//
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+
+                //….retrieve the BluetoothDevice object and its EXTRA_DEVICE field, which contains information about the device’s characteristics and capabilities//
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                //You’ll usually want to display information about any devices you discover, so here I’m adding each device’s name and address to an ArrayAdapter,
+                //which I’d eventually incorporate into a ListView//
+                if(device.getName() != null) {
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                    devicesListView.setAdapter(mArrayAdapter);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        devicesListView = findViewById(R.id.devices_list_view);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
             //Display a toast notifying the user that their device doesn’t support Bluetooth//
-            Toast.makeText(getApplicationContext(),"This device doesn’t support Bluetooth",Toast.LENGTH_LONG).show();
-        }
-        else {
+            Toast.makeText(getApplicationContext(), "This device doesn’t support Bluetooth", Toast.LENGTH_LONG).show();
+        } else {
             //If BluetoothAdapter doesn’t return null, then the device does support Bluetooth//
             Toast.makeText(getApplicationContext(), "This device does support Bluetooth", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void showPairedDevices(){
+    private void showPairedDevices() {
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        // Construct the data source
-        ArrayList<String> devices = new ArrayList<>();
-        // Create the adapter to convert the array to views
-        //UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
 
-
-        ArrayAdapter<String>  mArrayAdapter = new ArrayAdapter<>(this, R.layout.activity_main);
         // If there’s 1 or more paired devices...//
         if (pairedDevices.size() > 0) {
-
             //...then loop through these devices//
             for (BluetoothDevice device : pairedDevices) {
                 //Retrieve each device’s public identifier and MAC address. Add each device’s name and address to an ArrayAdapter, ready to incorporate into a
                 //ListView
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
             }
-
-
+            devicesListView.setAdapter(mArrayAdapter);
         }
     }
 
@@ -77,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //If the request was unsuccessful...//
-            if(resultCode == RESULT_CANCELED){
+            if (resultCode == RESULT_CANCELED) {
 
                 //...then display this alternative toast.//
                 Toast.makeText(getApplicationContext(), "An error occurred while attempting to enable Bluetooth",
@@ -90,12 +115,73 @@ public class MainActivity extends AppCompatActivity {
 
         if (!bluetoothAdapter.isEnabled()) {
             //Create an intent with the ACTION_REQUEST_ENABLE action, which we’ll use to display our system Activity//
-            android.content.Intent enableIntent = new android.content.Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 
             //Pass this intent to startActivityForResult(). ENABLE_BT_REQUEST_CODE is a locally defined integer that must be greater than 0,
             //for example private static final int ENABLE_BT_REQUEST_CODE = 1//
             startActivityForResult(enableIntent, ENABLE_BT_REQUEST_CODE);
             Toast.makeText(getApplicationContext(), "Enabling Bluetooth!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public void makeDiscoverable(View view) {
+
+        Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+
+        //Specify how long the device will be discoverable for, in seconds.//
+        discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 400);
+        startActivity(discoveryIntent);
+    }
+
+    public void discover(View view) {
+        if (bluetoothAdapter.isDiscovering()) {
+            // Bluetooth is already in modo discovery mode, we cancel to restart it again
+            bluetoothAdapter.cancelDiscovery();
+        }
+
+        //Checking BT permissions
+        checkBTPermissions();
+
+        //Register for the ACTION_FOUND broadcast//
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        //filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        //filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(broadcastReceiver, filter);
+
+        if (bluetoothAdapter.startDiscovery()) {
+            //If discovery has started, then display the following toast....//
+            Toast.makeText(getApplicationContext(), "Discovering other bluetooth devices...",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            //If discovery hasn’t started, then display this alternative toast//
+            Toast.makeText(getApplicationContext(), "Something went wrong! Discovery has failed to start.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        mArrayAdapter.clear();
+    }
+
+    /**
+     * This method is required for all devices running API23+
+     * Android must programmatically check the permissions for bluetooth. Putting the proper permissions
+     * in the manifest is not enough.
+     *
+     * NOTE: This will only execute on versions > LOLLIPOP because it is not needed otherwise.
+     */
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(broadcastReceiver);
+        super.onDestroy();
     }
 }
